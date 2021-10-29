b621545cedd267e82292605158ff8376f29b1dee
João Porto <joaov.aporto@gmail.com>
2021-10-29 03:47:44 -0300

    Initial solution to algo&log exercise 7

---
+++ graph.php
@@ -0,0 +1,118 @@
+<?php
+
+class Vertice {
+    public $id;
+    public $value;
+    public $neighbors;
+
+    public function __construct($value) {
+        $this->id = uniqid();
+        $this->value = $value;
+    }
+
+    public function add_neighbor($neighbor) {
+        $this->neighbors[$neighbor->id] = $neighbor;
+    }
+
+    public function rmv_neighbor($neighbor) {
+        unset($this->neighbors[$neighbor->id]);
+    }
+}
+
+class Link {
+    public $id;
+    public $vertice_1;
+    public $vertice_2;
+    public $value;
+
+    public function __construct($vertice_1, $vertice_2, $value = null) {
+        $this->id = Link::get_link_id($vertice_1, $vertice_2);
+        $this->value = $value;
+        $this->vertice_1 = $vertice_1;
+        $this->vertice_2 = $vertice_2;
+
+        $this->link_vertices();
+    }
+
+    public function __destruct() {
+        $this->unlink_vertices();
+    }
+
+    public static function get_link_id($vertice_1, $vertice_2) {
+        $link_id = $vertice_1->id . $vertice_2->id;
+        return $link_id;
+    }
+
+    private function link_vertices() {
+        $this->vertice_1->add_neighbor($this->vertice_2);
+        $this->vertice_2->add_neighbor($this->vertice_1);
+    }
+
+    private function unlink_vertices() {
+        $this->vertice_1->rmv_neighbor($this->vertice_2);
+        $this->vertice_2->rmv_neighbor($this->vertice_1);
+    }
+}
+
+class Graph {
+    public $vertices;
+
+    public function __construct() {
+        $this->vertices = [];
+    }
+
+    public function add_vertice($value) {
+        $vertice = new Vertice($value);
+        $this->vertices[$vertice->id] = $vertice;
+        return $vertice;
+    }
+
+    public function rmv_vertice($vertice_id) {
+        unset($this->vertices[$vertice_id]);
+    }
+
+    public function add_link($vertice_1, $vertice_2) {
+        $link = new Link($vertice_1, $vertice_2);
+        $this->links[$link->id] = $link;
+    }
+
+    public function rmv_link($vertice_1, $vertice_2) {
+        $link_id = Link::get_link_id($vertice_1, $vertice_2);
+        unset($this->links[$link_id]);
+    }
+
+    private function _get_paths_between_vertices($start_vertice,
+        $end_vertice,
+        $original_start_vertice,
+        $irrelevant_neighbor = null) {
+        $paths = [];
+            
+
+        foreach ($start_vertice->neighbors as $start_vertice_neighbor) {
+            if ($start_vertice_neighbor->id === $original_start_vertice->id) {
+                continue;
+            }
+
+            if (!is_null($irrelevant_neighbor) && $start_vertice_neighbor->id === $irrelevant_neighbor->id) {
+                continue;
+            }
+
+            if ($start_vertice_neighbor->id === $end_vertice->id) {
+                $paths[] = [$start_vertice, $end_vertice];
+            } else {
+                $_paths = $this->_get_paths_between_vertices($start_vertice_neighbor, $end_vertice, $original_start_vertice, $start_vertice);
+
+                foreach ($_paths as $path) {
+                    $paths[] = array_merge([$start_vertice], $path);
+                }
+            }
+        }
+
+        return $paths;
+    }
+
+    public function get_paths_between_vertices($start_vertice, $end_vertice) {
+        return $this->_get_paths_between_vertices($start_vertice, $end_vertice, $start_vertice);
+    }
+
+}
--- main.php
+++ main.php
@@ -4,6 +4,7 @@ require_once("./quicksort.php");
 require_once("./quicksort_decrescent.php");
 require_once("./geo.php");
 require_once("./functions.php");
+require_once("./graph.php");
 
 /**
  * Rotates a array to right based on a given offset.
@@ -132,6 +133,8 @@ function contains($string, $substring) {
     return false;
 }
 
+
+
 // print_r(rotate_to_right([1,2,3,4,5,6], 2));
 // print_r(sort_even_crescent_odd_decrescent([6,5,4,3,2,1]));
 // print_r(Date::calc_date_diff_in_days("03/04/1998", "27/10/2021"));
@@ -152,6 +155,37 @@ function contains($string, $substring) {
 // $rectangle_1 = new Rectangle(new Point(5,1), new Point(7,3));
 // $rectangle_2 = new Rectangle(new Point(6,-1), new Point(8,2));
 // print_r($rectangle_1->get_overlap_area($rectangle_2));
-$rectangle_1 = new Rectangle(new Point(5,1), new Point(7,3));
-$rectangle_2 = new Rectangle(new Point(6,-1), new Point(8,2));
-print_r($rectangle_1->get_overlap_area($rectangle_2));
+// $rectangle_1 = new Rectangle(new Point(5,1), new Point(7,3));
+// $rectangle_2 = new Rectangle(new Point(6,-1), new Point(8,2));
+// print_r($rectangle_1->get_overlap_area($rectangle_2));
+
+
+$graph = new Graph();
+$vertice_a = $graph->add_vertice("a");
+$vertice_b = $graph->add_vertice("b");
+$vertice_c = $graph->add_vertice("c");
+$vertice_d = $graph->add_vertice("d");
+$vertice_e = $graph->add_vertice("e");
+$vertice_f = $graph->add_vertice("f");
+$vertice_g = $graph->add_vertice("g");
+$vertice_h = $graph->add_vertice("h");
+$graph->add_link($vertice_a, $vertice_b);
+$graph->add_link($vertice_a, $vertice_e);
+$graph->add_link($vertice_a, $vertice_h);
+$graph->add_link($vertice_b, $vertice_c);
+$graph->add_link($vertice_b, $vertice_d);
+$graph->add_link($vertice_c, $vertice_d);
+$graph->add_link($vertice_c, $vertice_e);
+$graph->add_link($vertice_d, $vertice_g);
+$graph->add_link($vertice_e, $vertice_f);
+$graph->add_link($vertice_g, $vertice_h);
+$paths = $graph->get_paths_between_vertices($vertice_a, $vertice_c);
+$paths = array_map(function($path) {
+        $path = array_map(function($vertice) {
+            return $vertice->value;
+        }, $path);
+    return $path;
+}, $paths);
+print_r($paths);
+
+
